049786a95ce9fdaa8c697a67caa196e6fb5d31ae
jsjxsy <jsjxsy@gmail.com>
2016-10-08 20:51:06 +0800

    新增需求，新的支付功能

--- GameCatSDK1.0.1/app/src/main/java/com/youximao/demo/MainActivity.java
+++ GameCatSDK1.0.1/app/src/main/java/com/youximao/demo/MainActivity.java
@@ -1,15 +1,14 @@
 package com.youximao.demo;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.gamecat.pay.GameCatSDK;
-import com.gamecat.pay.GameCatSDKListener;
+import com.gamecat.sdk.GameCatSDK;
+import com.gamecat.sdk.GameCatSDKListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
